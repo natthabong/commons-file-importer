512e655f26a41034a0a93b003e62700fcf3c3d73
GEC\thanunchai.kun <thanunchai.kun@gec.co.th>
2016-10-10 21:15:12 +0700

    [NEW] เพิ่ม Errormessage

--- src/main/java/gec/scf/file/validator/DataCondition.java
+++ src/main/java/gec/scf/file/validator/DataCondition.java
@@ -3,6 +3,8 @@ package gec.scf.file.validator;
 public class DataCondition {
 
 	private ConditionType conditionType;
+	private String notExistErrorMessage;
+	private String inactiveErrorMessage;
 
 	public void setConditionType(ConditionType conditionType) {
 		this.conditionType = conditionType;
@@ -10,6 +12,22 @@ public class DataCondition {
 
 	public ConditionType getConditionType() {
 		return conditionType;
+	}
+
+	public String getNotExistErrorMessage() {
+		return notExistErrorMessage;
+	}
+
+	public String getInactiveErrorMessage() {
+		return inactiveErrorMessage;
+	}
+
+	public void setNotExistErrorMessage(String notExistErrorMessage) {
+		this.notExistErrorMessage = notExistErrorMessage;
+	}
+
+	public void setInactiveErrorMessage(String inactiveErrorMessage) {
+		this.inactiveErrorMessage = inactiveErrorMessage;
 	}
 
 }
