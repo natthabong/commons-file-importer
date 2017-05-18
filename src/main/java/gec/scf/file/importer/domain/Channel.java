696f78998f05a06871f97de027fd79956b47e90b
Natthapong Noosing <natthapong.noo@gec.co.th>
2017-05-18 16:42:40 +0700

    [FIXED] Channel type

--- src/main/java/gec/scf/file/importer/domain/Channel.java
+++ src/main/java/gec/scf/file/importer/domain/Channel.java
@@ -1,5 +1,17 @@
 package gec.scf.file.importer.domain;
 
 public enum Channel {
-	WEB,FTP,EMAIL
+	WEB(Values.WEB), FTP(Values.FTP), EMAIL(Values.EMAIL);
+
+	private Channel(String val) {
+		// force equality between name of enum instance, and value of constant
+		if (!this.name().equals(val))
+			throw new IllegalArgumentException("Incorrect use of Channel");
+	}
+
+	public static class Values {
+		public static final String WEB = "WEB";
+		public static final String FTP = "FTP";
+		public static final String EMAIL = "EMAIL";
+	}
 }
